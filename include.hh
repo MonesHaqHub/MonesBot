eae16dbf8d09cc264b7c4bdd739c4efcbc841997
MonesHaqHub <80607140+MonesHaqHub@users.noreply.github.com>
2021-03-14 04:23:33 +0100

    Initialize dev-restructure

---
+++ include.hh
@@ -0,0 +1,37 @@
+//
+// Created by Aidan on 10/2/2020.
+//
+
+#pragma once
+
+#ifndef ECHO_EXTERN
+
+#include <fstream>
+#include <iostream>
+#include <regex>
+
+#include <boost/asio.hpp>
+
+// Put more non-plugin `include`s here
+
+#endif
+
+#include <discordpp/bot.hh>
+#include <discordpp/plugin-overload.hh>
+#include <discordpp/plugin-ratelimit.hh>
+#include <discordpp/plugin-responder.hh>
+#include <discordpp/plugin-constructs.hh>
+#include <discordpp/plugin-endpoints.hh>
+#include <discordpp/rest-beast.hh>
+#include <discordpp/websocket-simpleweb.hh>
+
+// Put more D++ plugin `include`s here
+
+#ifndef ECHO_EXTERN
+
+extern template class discordpp::PluginEndpoints<discordpp::PluginConstructs<discordpp::PluginResponder<discordpp::PluginOverload<discordpp::PluginRateLimit<discordpp::WebsocketSimpleWeb<discordpp::RestBeast<discordpp::Bot>>>>>>>;
+using DppBot = discordpp::PluginEndpoints<discordpp::PluginConstructs<discordpp::PluginResponder<discordpp::PluginOverload<discordpp::PluginRateLimit<discordpp::WebsocketSimpleWeb<discordpp::RestBeast<discordpp::Bot>>>>>>>;
+
+#undef DPPBOTDEF
+
+#endif
